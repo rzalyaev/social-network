90cd359dc3e103c991da0607a3179f3b59b6d28d
rzalyaev <ruslan.zalyaev@gmail.com>
2024-02-24 23:37:16 +0400

    Create Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+      <div>
+        <a href="#">Main</a>
+        <a href="#">News</a>
+        <a href="#">Friends</a>
+      </div>
+  );
+};
+
+export default Header;
