f1d7519ddea632bbb80450c6d5fd2e90090febc6
rzalyaev <ruslan.zalyaev@gmail.com>
2024-02-26 23:01:41 +0400

    Create Profile component

---
+++ src/components/Content/ProfilePage/Profile/Profile.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Profile = () => {
+  return (
+      <div>
+        <div><img src="#" alt="profile avatar"/></div>
+        <div><h3>Ruslan Zalyaev</h3></div>
+        <p>DoB: 28 July 1995</p>
+        <p>City: Izhevsk</p>
+        <p>Education: secondary</p>
+        <p>Web site: <a href="https://github.com/rzalyaev"></a></p>
+      </div>
+  );
+};
