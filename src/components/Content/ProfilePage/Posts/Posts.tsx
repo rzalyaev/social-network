d9f196827e6a1cf6aff842e74d4df089bf4aced9
rzalyaev <ruslan.zalyaev@gmail.com>
2024-02-28 20:41:24 +0400

    Add send post functionality

--- src/components/Content/ProfilePage/Posts/Posts.tsx
+++ src/components/Content/ProfilePage/Posts/Posts.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {ChangeEvent, ChangeEventHandler, useState} from 'react';
+import styles from './Posts.module.css';
+import {Post} from "./Post/Post";
+
+type PostType = {
+  id: number
+  text: string
+}
 
 export const Posts = () => {
+  const posts: PostType[] = [
+    {id: 1, text: 'I want to be a programmer.'},
+    {id: 2, text: 'I like to listening music.'},
+    {id: 3, text: 'Hello, I am Ruslan.'}
+  ]
+  const [postsArray, setPostsArray] = useState(posts);
+  const addPost = (newPostText: string) => {
+    const newPost: PostType = {id: 4, text: newPostText};
+    setPostsArray([newPost, ...postsArray]);
+  }
+
+  const [currentPostText, setCurrentPostText] = useState('');
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => setCurrentPostText(event.currentTarget.value);
+  const onClickHandler = () => {
+    addPost(currentPostText);
+    setCurrentPostText('');
+  };
+
   return (
-      <div>
-        <h3>My posts</h3>
-        <div>
-          <textarea name="postText" id="1"></textarea>
-          <button>Send</button>
-        </div>
         <div>
           <div>
             <img src="#" alt="post author avatar"/>
@@ -21,6 +40,16 @@ export const Posts = () => {
             <img src="#" alt="post author avatar"/>
             <p>I'm sexy and I know it!</p>
           </div>
+      <div className={styles.postsWrapper}>
+        <div className={styles.sendPostForm}>
+          <input name="postText"
+                 id="1"
+                 value={currentPostText}
+                 onChange={onChangeHandler} className={styles.sendPostInput}></input>
+          {currentPostText.length > 0 &&
+              <button className={styles.sendPostButton} onClick={onClickHandler}>Send</button>}
+        </div>
+        <div>
         </div>
       </div>
   );
