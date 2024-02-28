14727e771bdf222ac59626d430c4057c978d1fc1
rzalyaev <ruslan.zalyaev@gmail.com>
2024-02-28 20:46:18 +0400

    Migrate profile banner to Profile component

--- src/components/Content/ProfilePage/ProfilePage.tsx
+++ src/components/Content/ProfilePage/ProfilePage.tsx
@@ -4,10 +4,7 @@ import {Posts} from "./Posts/Posts";
 
 export const ProfilePage = () => {
   return (
-      <div>
-        <div>
-          <img src="#" alt="profile banner"/>
-        </div>
+      <div className={styles.profilePage}>
         <Profile/>
         <Posts/>
       </div>
