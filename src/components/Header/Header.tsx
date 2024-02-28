e015f5826620f0832e5eb50cd4a003fb586e434d
rzalyaev <ruslan.zalyaev@gmail.com>
2024-02-28 20:47:34 +0400

    Add wrapper div to adjust styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,10 +4,12 @@ import styles from "./Header.module.css";
 const Header = () => {
   return (
       <header className={styles.header}>
-        <div className={styles.logoWrapper}>
-          <img src="https://i.imgur.com/BGCp8In.png" alt="logo" className={styles.logo}/>
+        <div className={styles.wrapper}>
+          <div className={styles.logoWrapper}>
+            <img src="https://i.imgur.com/BGCp8In.png" alt="logo" className={styles.logo}/>
+          </div>
+          <h1>Social Network</h1>
         </div>
-        <h1>Social Network</h1>
       </header>
   );
 };
