97efc058df26bbccbdcf9924351dac9502330c54
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-01 01:02:47 +0400

    Add Messenger component and comment ProfilePage (temporary)

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './Content.module.css';
 import {ProfilePage} from "./ProfilePage/ProfilePage";
+import {Messenger} from "./Messenger/Messenger";
 
 export const Content = () => {
   return (
       <div className={styles.content}>
-        <ProfilePage/>
+        {/*<ProfilePage/>*/}
+        <Messenger/>
       </div>
   );
 };
