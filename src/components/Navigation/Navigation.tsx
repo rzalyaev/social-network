04c036eb4d80c37d879ba72c1fbb0125c4fe83f1
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-03 23:58:24 +0400

    Add href values for links

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -4,11 +4,11 @@ import styles from './Navigation.module.css';
 export const Navigation = () => {
   return (
       <ul className={styles.navigation}>
-        <li className={styles.sidebarItem}><a href="">Profile</a></li>
-        <li className={styles.sidebarItem}><a href="">Messages</a></li>
-        <li className={styles.sidebarItem}><a href="">News</a></li>
-        <li className={styles.sidebarItem}><a href="">Music</a></li>
-        <li className={styles.sidebarItem}><a href="">Settings</a></li>
+        <li className={styles.sidebarItem}><a href="/profile">Profile</a></li>
+        <li className={styles.sidebarItem}><a href="/messages">Messages</a></li>
+        <li className={styles.sidebarItem}><a href="/news">News</a></li>
+        <li className={styles.sidebarItem}><a href="/music">Music</a></li>
+        <li className={styles.sidebarItem}><a href="/video">Settings</a></li>
       </ul>
   );
 };
