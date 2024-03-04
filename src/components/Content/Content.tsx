93e11377bd58bb994e6239344a44b14534af475a
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-04 21:20:50 +0400

    Add routes

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import styles from './Content.module.css';
 import {ProfilePage} from "./ProfilePage/ProfilePage";
 import {Messenger} from "./Messenger/Messenger";
+import {Route, Routes} from "react-router-dom";
 
 export const Content = () => {
   return (
       <div className={styles.content}>
-        {/*<ProfilePage/>*/}
-        <Messenger/>
+        <Routes>
+          <Route path={'/profile'} element={<ProfilePage/>}/>
+          <Route path={'/messenger'} element={<Messenger/>}/>
+        </Routes>
       </div>
   );
 };
