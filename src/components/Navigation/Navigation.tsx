cf21231ed719c8988a9bc3894506287a6399cfe3
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-04 21:19:07 +0400

    Change a tag to NavLink

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import styles from './Navigation.module.css';
+import {NavLink} from "react-router-dom";
 
 export const Navigation = () => {
   return (
       <ul className={styles.navigation}>
-        <li className={styles.sidebarItem}><a href="/profile">Profile</a></li>
-        <li className={styles.sidebarItem}><a href="/messages">Messages</a></li>
-        <li className={styles.sidebarItem}><a href="/news">News</a></li>
-        <li className={styles.sidebarItem}><a href="/music">Music</a></li>
-        <li className={styles.sidebarItem}><a href="/video">Settings</a></li>
+        <li className={styles.sidebarItem}><NavLink to="/profile">Profile</NavLink></li>
+        <li className={styles.sidebarItem}><NavLink to="/messenger">Messages</NavLink></li>
+        <li className={styles.sidebarItem}><NavLink to="/news">News</NavLink></li>
+        <li className={styles.sidebarItem}><NavLink to="/music">Music</NavLink></li>
+        <li className={styles.sidebarItem}><NavLink to="/video">Settings</NavLink></li>
       </ul>
   );
 };
