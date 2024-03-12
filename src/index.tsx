6a076424f9f1b23d1cc718f4895626147d398c86
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-12 22:14:31 +0400

    Add state as props to App

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
+import {state} from './state/state';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <BrowserRouter>
-      <App />
+      <App state={state}/>
     </BrowserRouter>
 );
 
