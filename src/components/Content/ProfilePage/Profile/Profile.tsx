53103e941d929418b04f4bc4c13d95c4924bc920
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-12 22:13:27 +0400

    Refactor for better code reading

--- src/components/Content/ProfilePage/Profile/Profile.tsx
+++ src/components/Content/ProfilePage/Profile/Profile.tsx
@@ -20,7 +20,10 @@ export const Profile = () => {
             <h2>Ruslan Zalyaev</h2>
             <p className={styles.dateOfBirth}>28 July 1995</p>
             <p><img src="https://cdn-icons-png.flaticon.com/512/0/619.png" alt="city icon"/> Izhevsk</p>
-            <p><img src="https://cdn-icons-png.freepik.com/256/1006/1006771.png" alt="web site icon"/> <a href="https://github.com/rzalyaev">https://github.com/rzalyaev</a></p>
+            <p>
+              <img src="https://cdn-icons-png.freepik.com/256/1006/1006771.png" alt="web site icon"/>
+              <a href="https://github.com/rzalyaev">https://github.com/rzalyaev</a>
+            </p>
           </div>
         </div>
       </div>
