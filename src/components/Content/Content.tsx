1a22a984daf2c61798a0b0576c9f970cb9cb20d4
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-13 23:57:03 +0400

    Change StateType import way

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './Content.module.css';
-import {ProfilePage} from "./ProfilePage/ProfilePage";
+import {Profile} from "./Profile/Profile";
 import {Messenger} from "./Messenger/Messenger";
 import {Navigate, Route, Routes} from "react-router-dom";
-import {StateType} from "../../state/state";
+import {StateType} from "../../redux/state";
 
 type ContentPropsType = {
   state: StateType
@@ -14,7 +14,7 @@ export const Content = ({state}: ContentPropsType) => {
       <div className={styles.content}>
         <Routes>
           <Route path={'/'} element={<Navigate to={'/profile'}/>}/>
-          <Route path={'/profile'} element={<ProfilePage profilePage={state.profilePage}/>}/>
+          <Route path={'/profile'} element={<Profile profilePage={state.profilePage}/>}/>
           <Route path={'/messenger'} element={<Messenger messengerPage={state.messengerPage}/>}/>
         </Routes>
       </div>
