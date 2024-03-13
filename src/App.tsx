ec2d1e1f2a5d9394662f526698aadcccf6d02891
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-13 23:57:17 +0400

    Change StateType import way

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import Header from "./components/Header/Header";
 import {Navigation} from "./components/Navigation/Navigation";
 import {Content} from "./components/Content/Content";
-import {StateType} from "./state/state";
+import {StateType} from "./redux/state";
 
 type AppPropsType = {
   state: StateType
