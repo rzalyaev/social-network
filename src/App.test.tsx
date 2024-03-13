ecdb5173125b67477c99311b8bb483bc92fc39fa
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-13 23:57:35 +0400

    Change state import way

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import {state} from "./state/state";
+import {state} from "./redux/state";
 
 test('renders learn react link', () => {
   render(<App state={state}/>);
