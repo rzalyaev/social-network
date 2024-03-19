4016c5a3d65b8adb0874edc7d8734c4bb320b13d
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-19 20:44:07 +0400

    Remove props from Content due to using redux

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,13 @@ import './App.css';
 import Header from "./components/Header/Header";
 import {Navigation} from "./components/Navigation/Navigation";
 import {Content} from "./components/Content/Content";
-import {StateType} from "./redux/store";
 
-type AppPropsType = {
-  state: StateType
-}
-
-function App({state}: AppPropsType) {
+function App() {
   return (
     <div className="App">
       <Header/>
       <Navigation/>
-      <Content state={state}/>
+      <Content/>
     </div>
   );
 }
