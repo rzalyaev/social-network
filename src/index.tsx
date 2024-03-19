c0daa7686da3ba07d6ce19316428321b43c77100
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-19 20:43:42 +0400

    Add Provider as a wrapper for App due to using react-redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {reduxStore} from './redux/reduxStore';
+import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
-import {store} from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <BrowserRouter>
-      <App state={store.getState()}/>
-    </BrowserRouter>
+    <Provider store={reduxStore}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
