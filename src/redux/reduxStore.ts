82e81888c9773813f0ff25be134d2de07c36ada7
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-26 20:16:06 +0400

    Add usersReducer to combineReducers

--- src/redux/reduxStore.ts
+++ src/redux/reduxStore.ts
@@ -1,10 +1,12 @@
 import {combineReducers, createStore} from "redux";
 import profileReducer from "./reducers/profileReducer";
 import messengerReducer from "./reducers/messengerReducer";
+import usersReducer from "./reducers/usersReducer";
 
 const rootReducer = combineReducers({
   profilePage: profileReducer,
-  messengerPage: messengerReducer
+  messengerPage: messengerReducer,
+  usersPage: usersReducer
 });
 
 export const reduxStore = createStore(rootReducer);
