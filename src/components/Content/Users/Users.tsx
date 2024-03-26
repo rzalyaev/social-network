3938d14f1cdd8d3cc02ebf0c55a6ad264f724f5c
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-26 20:14:06 +0400

    Create Users component

---
+++ src/components/Content/Users/Users.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Users.module.css';
+import {useDispatch, useSelector} from "react-redux";
+import {AppRootStateType} from "../../../redux/reduxStore";
+import {subscribeAC, unsubscribeAC, UsersPageType} from "../../../redux/reducers/userReducer";
+import User from './User/User';
+
+export const Users = () => {
+  const users = useSelector<AppRootStateType, UsersPageType>(state => state.usersPage);
+  const dispatch = useDispatch();
+  const subscribe = (userId: string) => dispatch(subscribeAC(userId));
+  const unsubscribe = (userId: string) => dispatch(unsubscribeAC(userId));
+  const mappedUsers = users.map(user => <User user={user} subscribe={subscribe} unsubscribe={unsubscribe}/>);
+  return (
+      <div className={styles.usersWrapper}>
+        {mappedUsers}
+      </div>
+  );
+};
